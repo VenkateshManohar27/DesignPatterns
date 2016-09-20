1ee24e2bdf590c433b9040e485b61f7085856fcc
VenkateshManohar27 <venkatesh.manohar@gmail.com>
2016-09-20 21:22:20 +0530

    Added a private constructor

--- DesignPatterns/src/com/vm/designpatterns/EagerInitializedSingleton.java
+++ DesignPatterns/src/com/vm/designpatterns/EagerInitializedSingleton.java
@@ -1,5 +1,13 @@
 package com.vm.designpatterns;
 
 public class EagerInitializedSingleton {
-
+	private static EagerInitializedSingleton singleton = new EagerInitializedSingleton();
+	
+	private EagerInitializedSingleton(){
+		
+	}
+	
+	public static EagerInitializedSingleton getInstance(){
+		return singleton;
+	}
 }
